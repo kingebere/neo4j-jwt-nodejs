c40a1584f8c843894892f0b1a32ba148cddcc9ac
fadimoubassaleh <fadimoubassaleh@gmail.com>
2019-09-01 17:05:12 +0300

    create .env

--- app.js
+++ app.js
@@ -2,22 +2,24 @@ const express = require('express');
 const app = express();
 const neo4j = require('neo4j-driver').v1;
 const userAPI = require('./routes/user'); // import user apis
+const dotenv = require('dotenv'); // env
+
+dotenv.config();
+
 // neo4j setup
 var driver = neo4j.driver(
     'bolt://localhost',
-    neo4j.auth.basic('neo4j', 'password')
+    neo4j.auth.basic(process.env.NEO_USERNAME, process.env.NEO_PASSWORD)
 )
 const session = driver.session()
     // 
 
 // test neo4j
-app.get('/test', function(req, res) {
+app.post('/test', function(req, res) {
         session
             .run('CREATE (user:Person {email:{emailParam}, password:{passwordParam}}) RETURN user', { emailParam: 'ask@fadi.solutions', passwordParam: '1234' })
             .then(function(result) {
-                res.sendStatus(200).json({
-                    success: true
-                })
+                res.sendStatus(200)
                 console.log('result')
                 session.close();
                 driver.close();
